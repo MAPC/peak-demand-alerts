640fb306bbada1df6b5b0fc80b2d2c80c8dca8e1
Matthew Zagaja <mzagaja@mapc.org>
2020-03-03 16:06:43 -0500

    Remove Unused Test Suite
    Remove unused MiniTest test suite.

--- test/mailers/previews/report_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/report
-class ReportPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/report/daily
-  def daily
-    ReportMailer.daily(Report.last)
-  end
-
-end
